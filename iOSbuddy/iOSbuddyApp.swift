a76e04c64429f1c57c0a9f21c4cf53af9b5bd685
DumDumm <dumdumm21@gmail.com>
2025-04-20 08:03:20 -0400

    Add Xcode project files and source code

---
+++ iOSbuddy/iOSbuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSbuddyApp.swift
+//  iOSbuddy
+//
+//  Created by Adam Kalita on 4/20/25.
+//
+
+import SwiftUI
+
+@main
+struct iOSbuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOSbuddyTests/iOSbuddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOSbuddyTests.swift
+//  iOSbuddyTests
+//
+//  Created by Adam Kalita on 4/20/25.
+//
+
+import Testing
+@testable import iOSbuddy
+
+struct iOSbuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
